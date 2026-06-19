9936d6d4e422ad5c7c7efb2ecc2d9fef1e5512d0
Virlan Adrian <119500963+ady12344@users.noreply.github.com>
2026-06-19 22:57:21 +0300

    Add: quickSort - submission-0

---
+++ Data Structures & Algorithms/quickSort/submission-0.java
@@ -0,0 +1,34 @@
+// Definition for a pair.
+// class Pair {
+//     int key;
+//     String value;
+//
+//     public Pair(int key, String value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+class Solution {
+    public List<Pair> quickSort(List<Pair> pairs) {
+        //aight we cooking
+        return quickSortHelper(pairs , 0 , pairs.size() - 1);
+    }
+    public List<Pair> quickSortHelper(List<Pair> pairs , int start, int end){
+        if(end - start + 1 <= 1) return pairs;
+        Pair pivot = pairs.get(end);
+        int leftPtr = start;
+        for(int i = start; i < end ; i++){
+            if(pivot.key > pairs.get(i).key){
+                Pair temp = pairs.get(leftPtr);
+                pairs.set(leftPtr, pairs.get(i));
+                pairs.set(i , temp);
+                leftPtr++;
+            }
+        }
+        pairs.set(end , pairs.get(leftPtr));
+        pairs.set(leftPtr, pivot);
+        quickSortHelper(pairs , start , leftPtr - 1);
+        quickSortHelper(pairs, leftPtr+1 , end);
+        return pairs;
+    }
+}
