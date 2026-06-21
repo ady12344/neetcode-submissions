b99d8eb573c7cf2e959d9a1deaa76861bc9cbc2c
Virlan Adrian <119500963+ady12344@users.noreply.github.com>
2026-06-21 15:40:22 +0300

    Add: k-closest-points-to-origin - submission-8

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-8.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] result = new int[k][2];
+        helper(points , 0 , points.length - 1 , k);
+        for(int i = 0 ; i < k ; i++){
+            result[i] = points[i];
+        }
+        return result;
+    }
+    public void helper(int[][] points, int start, int end , int k){
+        if(end - start + 1 <= 1) return;
+        int[] pivot = points[end];
+        int pivotDist = (int)(Math.pow(pivot[0] , 2) + Math.pow(pivot[1] , 2));
+        int left = start;
+        for(int i = start ; i < end ; i++){
+            int[] point = points[i];
+            int pointDist = (int)(Math.pow(point[0] , 2) + Math.pow(point[1] , 2));
+            if(pivotDist > pointDist){
+                int[] temp = point;
+                points[i] = points[left];
+                points[left] = temp;
+                left++;
+            }
+        }
+        points[end] = points[left];
+        points[left] = pivot;
+        //helper(points , start, left - 1);
+        //helper(points , left + 1 , end);
+        if (left > k) {
+            helper(points, start, left - 1, k);
+         } else {
+            helper(points, left + 1,end, k);
+        }
+    }
+}
